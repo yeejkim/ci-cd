d9790b2011d8048bf3d70c521d7faa279fa6456e
Kim YeJin <yejinkim00@swu.ac.kr>
2025-04-09 10:12:10 +0900

    Fix: updated test

--- step07_CICD/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_CICD/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -2,12 +2,19 @@ package edu.fisa.ce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;  // JUnit 5
 
 @SpringBootTest
 class Step07CicdApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int result = 2 + 3;
+		
+		System.out.println("비교 전***********");
+		assertEquals(5, result);  
+		
+		System.out.println("비교 후***********");
 	}
 
 }
